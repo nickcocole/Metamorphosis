f7576069255e0479102759776bb240256784099e
lee-ryan-lee <felipe.paes2@estudante.ifms.edu.br>
2026-06-10 11:56:58 -0400

    configuração do menu inicial: background, botoes jogar e encerrar

--- NetBeansProjects/com.mycompany_Metamorphosis_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/metamorphosis/controller/MenuPrincipalController.java
+++ NetBeansProjects/com.mycompany_Metamorphosis_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/metamorphosis/controller/MenuPrincipalController.java
@@ -2,13 +2,156 @@ package com.mycompany.metamorphosis.controller;
 
 import com.mycompany.metamorphosis.App;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class MenuPrincipalController {
     
     @FXML
     private ImageView background;
+    
+    @FXML
+    private ImageView btnJogar;
+    @FXML
+    private ImageView btnSair;
+    @FXML
+    private ImageView btnRanking;
+    @FXML
+    private ImageView btnCreditos;
+
+    private Image jogarNormal;
+    private Image jogarPressionado;
+    private Image sairNormal;
+    private Image sairPressionado;
+    private Image rankingNormal;
+    private Image rankingPressionado;
+    private Image creditosNormal;
+    private Image creditosPressionado;
+    
+    private double larguraOriginal;
+    private double alturaOriginal;
+    private double xOriginal;
+    private double yOriginal;
+
+    @FXML
+    public void initialize() {
+
+        // Carrega as imagens
+        jogarNormal = new Image(
+                getClass().getResourceAsStream("/stages/jogarnormal.png"));
+
+        jogarPressionado = new Image(
+                getClass().getResourceAsStream("/stages/jogarpressionado.png"));
+
+        // Define a imagem inicial
+        btnJogar.setImage(jogarNormal);
+
+        // Guarda os valores originais
+        larguraOriginal = btnJogar.getFitWidth();
+        alturaOriginal = btnJogar.getFitHeight();
+
+        xOriginal = btnJogar.getLayoutX();
+        yOriginal = btnJogar.getLayoutY();
+
+        // Mouse entrou no botão
+        btnJogar.setOnMouseEntered(e -> {
+
+            btnJogar.setFitWidth(larguraOriginal + 2);
+            btnJogar.setFitHeight(alturaOriginal + 2);
+
+            btnJogar.setLayoutX(xOriginal - 1);
+            btnJogar.setLayoutY(yOriginal - 1);
+        });
+
+        // Mouse saiu do botão
+        btnJogar.setOnMouseExited(e -> {
+
+            btnJogar.setFitWidth(larguraOriginal);
+            btnJogar.setFitHeight(alturaOriginal);
+
+            btnJogar.setLayoutX(xOriginal);
+            btnJogar.setLayoutY(yOriginal);
+
+            btnJogar.setImage(jogarNormal);
+        });
+
+        // Botão pressionado
+        btnJogar.setOnMousePressed(e ->
+                btnJogar.setImage(jogarPressionado)
+        );
+
+        // Botão solto
+        btnJogar.setOnMouseReleased(e -> {
+
+            btnJogar.setImage(jogarNormal);
+
+            try {
+                jogar();
+            } catch (IOException ex) {
+                Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        
+                // Carrega as imagens
+        sairNormal = new Image(
+                getClass().getResourceAsStream("/stages/sairnormal.png"));
+
+        //sairPressionado = new Image(
+                //getClass().getResourceAsStream("/stages/sairpressionado.png"));
+
+        // Define a imagem inicial
+        btnSair.setImage(sairNormal);
+
+        // Guarda os valores originais
+        double larguraOriginalSair = btnSair.getFitWidth();
+        double alturaOriginalSair = btnSair.getFitHeight();
+
+        double xOriginalSair = btnSair.getLayoutX();
+        double yOriginalSair = btnSair.getLayoutY();
+
+        // Mouse entrou no botão
+        btnSair.setOnMouseEntered(e -> {
+
+            btnSair.setFitWidth(larguraOriginalSair + 2);
+            btnSair.setFitHeight(alturaOriginalSair + 2);
+
+            btnSair.setLayoutX(xOriginalSair - 1);
+            btnSair.setLayoutY(yOriginalSair - 1);
+        });
+
+        // Mouse saiu do botão
+        btnSair.setOnMouseExited(e -> {
+
+            btnSair.setFitWidth(larguraOriginalSair);
+            btnSair.setFitHeight(alturaOriginalSair);
+
+            btnSair.setLayoutX(xOriginalSair);
+            btnSair.setLayoutY(yOriginalSair);
+
+            btnSair.setImage(sairNormal);
+        });
+
+        // Botão pressionado
+        /*btnSair.setOnMousePressed(e ->
+                btnSair.setImage(sairPressionado)
+        );*/
+
+        // Botão solto
+        btnSair.setOnMouseReleased(e -> {
+
+            btnSair.setImage(sairNormal);
+
+                    try {
+                        encerrar();
+                    } catch (IOException ex) {
+                        Logger.getLogger(MenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+        });
+        
+    }
 
     @FXML
     private void jogar() throws IOException {
